89a7398c73c620236109ecd3de1513736f34b768
Manivannan <manivannan@businessgateways.com>
2025-07-13 14:19:00 +0530

    mail application changes done

--- src/main/java/com/testapi/TestapiApplication.java
+++ src/main/java/com/testapi/TestapiApplication.java
@@ -1,9 +1,12 @@
 package com.testapi;
 
+import org.springframework.boot.SpringApplication;
+
 //import org.springframework.boot.SpringApplication;
 //import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //@SpringBootApplication
+
 //public class TestapiApplication {
 //
 //	public static void main(String[] args) {
@@ -12,17 +15,18 @@ package com.testapi;
 //
 //}
 
-
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class TestapiApplication extends SpringBootServletInitializer {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-            return application.sources(TestapiApplication.class); // Replace DemoApplication with your main class
-    }
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TestapiApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestapiApplication.class, args);
+	}
 
 }
-
-
