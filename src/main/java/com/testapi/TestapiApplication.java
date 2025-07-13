2a8f049f481d94a410f7ca9fa40f4eaefbb18653
Manivannan <manivannan@businessgateways.com>
2025-07-13 14:15:00 +0530

    convert jar file application to war file

--- src/main/java/com/testapi/TestapiApplication.java
+++ src/main/java/com/testapi/TestapiApplication.java
@@ -1,13 +1,28 @@
 package com.testapi;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class TestapiApplication {
+//@SpringBootApplication
+//public class TestapiApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(TestapiApplication.class, args);
+//	}
+//
+//}
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestapiApplication.class, args);
-	}
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class TestapiApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+            return application.sources(TestapiApplication.class); // Replace DemoApplication with your main class
+    }
 
 }
+
+
